535fb0ceeebdc089929bdedf7817e89648373e3d
S I P U N <sipunkumar85@gmail.com>
2023-08-17 02:54:24 +0530

    crowdin: Fixup Setup script

--- .github/scripts/setup-crowdin.sh
+++ .github/scripts/setup-crowdin.sh
@@ -4,9 +4,10 @@
             wait
           wget https://artifacts.crowdin.com/repo/deb/crowdin3.deb -O crowdin.deb
           sudo dpkg -i crowdin.deb
+          sudo apt-get install python3-git python3-yaml python3-lxml
           wait
           git clone https://github.com/PixysOS/crowdin.git crowdin
           cd crowdin
-          pip install -r requirements.txt
+          sudo pip install -r requirements.txt --user
           wait
           cd ~
